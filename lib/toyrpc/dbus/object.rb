2e81ff034d6805072e96a44fa4cfcbc55e996022
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 20:31:59 +0500

    Improve code

--- lib/toyrpc/dbus/object.rb
+++ lib/toyrpc/dbus/object.rb
@@ -41,15 +41,9 @@ module ToyRPC
         dbus_interface_name = dbus_message.interface.to_sym
         dbus_method_name    = dbus_message.member.to_sym
 
-        if intfs[dbus_interface_name].nil?
-          raise(
-            ::DBus.error('org.freedesktop.DBus.Error.UnknownMethod'),
-            "Interface \"#{dbus_interface_name}\" " \
-            "of object \"#{dbus_object_path}\" doesn't exist",
-          )
-        end
+        dbus_interface = get_interface_info(dbus_message)
 
-        if intfs[dbus_interface_name].methods[dbus_method_name].nil?
+        if dbus_interface.methods[dbus_method_name].nil?
           raise(
             ::DBus.error('org.freedesktop.DBus.Error.UnknownMethod'),
             "Method \"#{dbus_method_name}\" " \
@@ -58,7 +52,22 @@ module ToyRPC
           )
         end
 
-        intfs[dbus_interface_name].methods[dbus_method_name]
+        dbus_interface.methods[dbus_method_name]
+      end
+
+      def get_interface_info(dbus_message)
+        dbus_object_path    = dbus_message.path.to_s
+        dbus_interface_name = dbus_message.interface.to_sym
+
+        if intfs[dbus_interface_name].nil?
+          raise(
+            ::DBus.error('org.freedesktop.DBus.Error.UnknownMethod'),
+            "Interface \"#{dbus_interface_name}\" " \
+            "of object \"#{dbus_object_path}\" doesn't exist",
+          )
+        end
+
+        intfs[dbus_interface_name]
       end
     end
   end
