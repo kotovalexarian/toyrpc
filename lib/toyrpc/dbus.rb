2f8d849c5dd8bc5b261eaf9252eadf0f15825f49
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 20:29:45 +0500

    Add methods

--- lib/toyrpc/dbus.rb
+++ lib/toyrpc/dbus.rb
@@ -6,3 +6,17 @@ require 'toyrpc/dbus/interface'
 require 'toyrpc/dbus/method'
 require 'toyrpc/dbus/object'
 require 'toyrpc/dbus/param'
+
+module ToyRPC
+  module DBus
+    SYSTEM_SOCKET_NAME = 'unix:path=/var/run/dbus/system_bus_socket'
+
+    def self.system_socket_name
+      SYSTEM_SOCKET_NAME
+    end
+
+    def self.session_socket_name
+      ::DBus::SessionBus.session_bus_address
+    end
+  end
+end
