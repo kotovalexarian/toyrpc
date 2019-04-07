f97dcf27665b096ad623c6ef405c112c272f8377
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 21:13:20 +0500

    Override bus methods

--- lib/toyrpc/dbus/bus.rb
+++ lib/toyrpc/dbus/bus.rb
@@ -9,6 +9,39 @@ module ToyRPC
         super
         send_hello
       end
+
+      def request_service(name)
+        proxy.RequestName name, NAME_FLAG_REPLACE_EXISTING do |rmsg, r|
+          raise rmsg if rmsg.is_a? ::DBus::Error
+          unless r == REQUEST_NAME_REPLY_PRIMARY_OWNER
+            raise ::DBus::NameRequestError
+          end
+        end
+
+        @service = ::DBus::Service.new name, self
+        @service
+      end
+
+    private
+
+      def send_hello
+        send_sync hello_message do |rmsg|
+          @unique_name = rmsg.destination
+          ::DBus.logger.debug \
+            "Got hello reply. Our unique_name is #{@unique_name}"
+        end
+
+        @service = ::DBus::Service.new @unique_name, self
+      end
+
+      def hello_message
+        ::DBus::Message.new(::DBus::Message::METHOD_CALL).tap do |m|
+          m.destination = 'org.freedesktop.DBus'
+          m.path = '/org/freedesktop/DBus'
+          m.interface = 'org.freedesktop.DBus'
+          m.member = 'Hello'
+        end
+      end
     end
   end
 end
