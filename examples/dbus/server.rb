ef5de587abaf8cf0d085e27dc840c21860214e91
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 17:19:30 +0500

    Improve code

--- examples/dbus/server.rb
+++ examples/dbus/server.rb
@@ -3,29 +3,57 @@
 
 require 'dbus'
 
-class MyHandler
-  def add(left, right)
-    left + right
-  end
+class DBusInterface
+  attr_reader :name, :signals, :methods
 
-  def sub(left, right)
-    left - right
-  end
-
-  def mul(left, right)
-    left * right
+  def initialize(name:, signals:, methods:)
+    @name = name
+    @signals = signals
+    @methods = methods
   end
 end
 
-my_handler = MyHandler.new
+class DBusMethod
+  attr_reader :name, :ins, :outs
 
-class MyHandlerDBusObject
-  attr_reader :path
+  def initialize(name:, ins:, outs:)
+    @name = name
+    @ins = ins
+    @outs = outs
+  end
+
+  def types
+    outs.values
+  end
+
+  def to_xml
+    "<method name=\"#{name}\">\n" \
+    "#{ins_to_xml}"               \
+    "#{outs_to_xml}"              \
+    "</method>\n"
+  end
+
+  def ins_to_xml
+    ins.map do |name, type|
+      "<arg name=\"#{name}\" direction=\"in\" type=\"#{type}\"/>\n"
+    end.join
+  end
+
+  def outs_to_xml
+    outs.map do |name, type|
+      "<arg name=\"#{name}\" direction=\"out\" type=\"#{type}\"/>\n"
+    end.join
+  end
+end
+
+class DBusObject
+  attr_reader :path, :intfs
   attr_writer :service
 
-  def initialize(path, my_handler)
+  def initialize(path, handler, intfs)
     @path = path
-    @my_handler = my_handler
+    @handler = handler
+    @intfs = intfs
     @service = nil
   end
 
@@ -35,82 +63,11 @@ class MyHandlerDBusObject
     @service.bus.message_queue.push(reply(dbus_message))
   end
 
-  def intfs
-    INTERFACES
-  end
-
 private
-
-  class DBusInterface
-    attr_reader :name, :signals, :methods
-
-    def initialize(name:, signals:, methods:)
-      @name = name
-      @signals = signals
-      @methods = methods
-    end
-  end
-
-  class DBusMethod
-    attr_reader :name, :ins, :outs
-
-    def initialize(name:, ins:, outs:)
-      @name = name
-      @ins = ins
-      @outs = outs
-    end
-
-    def types
-      outs.values
-    end
-
-    def to_xml
-      "<method name=\"#{name}\">\n" \
-      "#{ins_to_xml}"               \
-      "#{outs_to_xml}"              \
-      "</method>\n"
-    end
-
-    def ins_to_xml
-      ins.map do |name, type|
-        "<arg name=\"#{name}\" direction=\"in\" type=\"#{type}\"/>\n"
-      end.join
-    end
-
-    def outs_to_xml
-      outs.map do |name, type|
-        "<arg name=\"#{name}\" direction=\"out\" type=\"#{type}\"/>\n"
-      end.join
-    end
-  end
-
-  INTERFACES = {
-    'com.example.MyHandler': DBusInterface.new(
-      name:    'com.example.MyHandler',
-      signals: {}.freeze,
-      methods: {
-        add: DBusMethod.new(
-          name: :add,
-          ins:  { left: 'i', right: 'i' },
-          outs: { result: 'i' },
-        ).freeze,
-        sub: DBusMethod.new(
-          name: :sub,
-          ins:  { left: 'i', right: 'i' },
-          outs: { result: 'i' },
-        ).freeze,
-        mul: DBusMethod.new(
-          name: :mul,
-          ins:  { left: 'i', right: 'i' },
-          outs: { result: 'i' },
-        ).freeze,
-      }.freeze,
-    ).freeze,
-  }.freeze
 
   def reply(dbus_message)
     method_info = get_method_info(dbus_message)
-    result = [*method(method_info.name).call(*dbus_message.params)]
+    result = [*@handler.method(method_info.name).call(*dbus_message.params)]
     reply = ::DBus::Message.method_return(dbus_message)
     method_info.types.zip(result).each do |type, data|
       reply.add_param(type, data)
@@ -126,7 +83,7 @@ private
     dbus_interface_name = dbus_message.interface.to_sym
     dbus_method_name    = dbus_message.member.to_sym
 
-    if INTERFACES[dbus_interface_name].nil?
+    if intfs[dbus_interface_name].nil?
       raise(
         ::DBus.error('org.freedesktop.DBus.Error.UnknownMethod'),
         "Interface \"#{dbus_interface_name}\" " \
@@ -134,7 +91,7 @@ private
       )
     end
 
-    if INTERFACES[dbus_interface_name].methods[dbus_method_name].nil?
+    if intfs[dbus_interface_name].methods[dbus_method_name].nil?
       raise(
         ::DBus.error('org.freedesktop.DBus.Error.UnknownMethod'),
         "Method \"#{dbus_method_name}\" " \
@@ -143,21 +100,49 @@ private
       )
     end
 
-    INTERFACES[dbus_interface_name].methods[dbus_method_name]
+    intfs[dbus_interface_name].methods[dbus_method_name]
   end
+end
 
+class MyHandler
   def add(left, right)
-    @my_handler.add(left, right)
+    left + right
   end
 
   def sub(left, right)
-    @my_handler.sub(left, right)
+    left - right
   end
 
   def mul(left, right)
-    @my_handler.mul(left, right)
+    left * right
   end
 end
+
+INTERFACES = {
+  'com.example.MyHandler': DBusInterface.new(
+    name:    'com.example.MyHandler',
+    signals: {}.freeze,
+    methods: {
+      add: DBusMethod.new(
+        name: :add,
+        ins:  { left: 'i', right: 'i' },
+        outs: { result: 'i' },
+      ).freeze,
+      sub: DBusMethod.new(
+        name: :sub,
+        ins:  { left: 'i', right: 'i' },
+        outs: { result: 'i' },
+      ).freeze,
+      mul: DBusMethod.new(
+        name: :mul,
+        ins:  { left: 'i', right: 'i' },
+        outs: { result: 'i' },
+      ).freeze,
+    }.freeze,
+  ).freeze,
+}.freeze
+
+my_handler = MyHandler.new
 
 dbus_socket_name = ARGV[0].to_s.strip
 
@@ -169,7 +154,7 @@ dbus_bus = if dbus_socket_name.empty?
 
 dbus_service = dbus_bus.request_service 'com.example.MyHandler'
 
-dbus_object = MyHandlerDBusObject.new '/com/example/MyHandler', my_handler
+dbus_object = DBusObject.new '/com/example/MyHandler', my_handler, INTERFACES
 
 dbus_service.export dbus_object
 
