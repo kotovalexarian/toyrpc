247c256f87c6d72a12e560422b66f1d5de558394
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 21:43:26 +0500

    Add executable to run daemon

--- lib/toyrpc.rb
+++ lib/toyrpc.rb
@@ -1,23 +1,3 @@
 # frozen_string_literal: true
 
 require 'toyrpc/version'
-
-##
-# Modern modular JSON-RPC client and server in Ruby.
-#
-module ToyRPC
-  DEFAULT_DBUS_DAEMON_CONFIG_FILE = File.expand_path(
-    File.join('..', 'fixtures', 'default.dbus.xml'),
-    __dir__,
-  ).freeze
-
-  def self.dbus_daemon(config_file = DEFAULT_DBUS_DAEMON_CONFIG_FILE)
-    Kernel.system('dbus-daemon',
-                  '--nofork',
-                  '--nopidfile',
-                  '--nosyslog',
-                  '--print-address', '',
-                  '--print-pid', '',
-                  '--config-file', config_file)
-  end
-end
