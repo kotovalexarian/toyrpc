0dd5b653aa60c5fa50b9c4a9662ddddd1b9b76e3
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 21:46:24 +0500

    Rename config

--- examples/dbus/daemon.rb
+++ examples/dbus/daemon.rb
@@ -1,15 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-DEFAULT_DBUS_DAEMON_CONFIG_FILE = File.expand_path(
-  File.join('..', '..', 'fixtures', 'default.dbus.xml'),
-  __dir__,
-).freeze
+DEFAULT_DBUS_DAEMON_CONFIG_FILE = File.expand_path('config.xml', __dir__).freeze
 
 Kernel.system('dbus-daemon',
               '--nofork',
               '--nopidfile',
               '--nosyslog',
               '--print-address', '',
-              '--print-pid', '',
               '--config-file', DEFAULT_DBUS_DAEMON_CONFIG_FILE)
