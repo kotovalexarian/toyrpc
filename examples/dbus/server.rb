d38249a77ccc485eef6acaa4506f23f606e449ae
Alex Kotov <kotovalexarian@gmail.com>
2019-04-07 17:46:33 +0500

    Add class DBusParam

--- examples/dbus/server.rb
+++ examples/dbus/server.rb
@@ -64,6 +64,53 @@ class DBusMethod
   end
 end
 
+class DBusParam
+  DIRECTIONS = %i[in out].freeze
+  TYPES = %i[i].freeze
+
+  attr_reader :name, :direction, :type
+
+  def initialize(name:, direction:, type:)
+    self.name = name
+    self.direction = direction
+    self.type = type
+  end
+
+  def to_xml
+    "<arg name=\"#{name}\" direction=\"#{direction}\" type=\"#{type}\"/>"
+  end
+
+  def in?
+    direction == :in
+  end
+
+  def out?
+    direction == :out
+  end
+
+private
+
+  def name=(value)
+    unless value.is_a? Symbol
+      raise TypeError, "Expected #{Symbol}, got #{value.class}"
+    end
+
+    @name = value
+  end
+
+  def direction=(value)
+    raise "Invalid value: #{value.inspect}" unless DIRECTIONS.include? value
+
+    @direction = value
+  end
+
+  def type=(value)
+    raise "Invalid value: #{value.inspect}" unless TYPES.include? value
+
+    @type = value
+  end
+end
+
 class DBusObject
   attr_reader :path, :intfs
   attr_writer :service
