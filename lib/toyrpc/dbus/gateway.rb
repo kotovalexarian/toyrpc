3c88366c5edf56fd312aa2d45a9146947aa14e30
Alex Kotov <kotovalexarian@gmail.com>
2019-04-12 03:07:04 +0500

    Remove unnecessary code

--- lib/toyrpc/dbus/gateway.rb
+++ lib/toyrpc/dbus/gateway.rb
@@ -14,38 +14,6 @@ module ToyRPC
         @proxies = {}
       end
 
-      def proxy(name)
-        unless name.instance_of? Symbol
-          raise TypeError, "Expected #{Symbol}, got #{name}"
-        end
-
-        @proxies[name] or raise "Unknown proxy: #{name.to_s.inspect}"
-      end
-
-      def add_proxy(name)
-        unless name.instance_of? Symbol
-          raise TypeError, "Expected #{Symbol}, got #{name.class}"
-        end
-
-        @proxies_mutex.synchronize do
-          unless @proxies[name].nil?
-            raise "Proxy name already in use: #{name.to_s.inspect}"
-          end
-
-          @proxies[name] = yield bus
-        end
-
-        nil
-      end
-
-      def add_proxy_class(name, klass)
-        unless klass.instance_of? Class
-          raise TypeError, "Expected #{Class}, got #{klass.class}"
-        end
-
-        add_proxy name, &klass.method(:new)
-      end
-
     private
 
       def address=(value)
