538307ece80b86bb40176b9063aeb45996e67a7a
Alex Kotov <kotovalexarian@gmail.com>
2019-04-12 03:44:27 +0500

    Remove unnecessary code

--- lib/toyrpc/dbus/gateway.rb
+++ lib/toyrpc/dbus/gateway.rb
@@ -6,22 +6,7 @@ module ToyRPC
       attr_reader :bus
 
       def initialize(address, handler)
-        self.address = address
-
         @bus = Bus.new address, handler
-
-        @proxies_mutex = Mutex.new
-        @proxies = {}
-      end
-
-    private
-
-      def address=(value)
-        unless value.instance_of? Address
-          raise TypeError, "Expected #{Address}, got #{value.class}"
-        end
-
-        @address = value.frozen? ? value : value.freeze
       end
     end
   end
