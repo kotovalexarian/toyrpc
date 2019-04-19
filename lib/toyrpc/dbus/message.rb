19a96f22556bba1a11e2886a4732ff83b0305b10
Alex Kotov <kotovalexarian@gmail.com>
2019-04-20 01:12:07 +0500

    Set nil attributes

--- lib/toyrpc/dbus/message.rb
+++ lib/toyrpc/dbus/message.rb
@@ -6,11 +6,13 @@ module ToyRPC
       def self.method_call(sender, # rubocop:disable Metrics/ParameterLists
                            destination, path, interface, member, *args)
         ::DBus::Message.new(::DBus::Message::METHOD_CALL).tap do |m|
-          m.sender      = sender
-          m.destination = destination
-          m.path        = path
-          m.interface   = interface
-          m.member      = member
+          m.reply_serial = nil
+          m.sender       = sender
+          m.destination  = destination
+          m.path         = path
+          m.interface    = interface
+          m.member       = member
+          m.error_name   = nil
 
           args.each do |a|
             m.add_param a[0], a[1]
@@ -21,7 +23,12 @@ module ToyRPC
       def self.reply_to(call_message, params)
         ::DBus::Message.new(::DBus::Message::METHOD_RETURN).tap do |m|
           m.reply_serial = call_message.serial
+          m.sender       = nil
           m.destination  = call_message.sender
+          m.path         = nil
+          m.interface    = nil
+          m.member       = nil
+          m.error_name   = nil
 
           params.each do |type, data|
             m.add_param type.to_s, data
@@ -32,7 +39,11 @@ module ToyRPC
       def self.reply_with_exception(call_message, exception)
         ::DBus::Message.new(::DBus::ERROR).tap do |m|
           m.reply_serial = call_message.serial
+          m.sender       = nil
           m.destination  = call_message.sender
+          m.path         = nil
+          m.interface    = nil
+          m.member       = nil
 
           m.error_name = if exception.is_a? ::DBus::Error
                            exception.name
